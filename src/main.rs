c11e50eedd934ed2c470a57403800822cf23b691
movodehe <movodehe@web.de>
2018-04-14 14:53:55 +0200

    combined crypt funktions and fixed filenaming bug

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,17 @@ fn main() {
         Ok(args) => args,
         Err(e) => return standard_io_error_handling(e),
     };
-    if encrypted {
-        match decrypt_and_save(file, filename, key) {
-            Ok(_) => println!("Success! You can find the decrypted file in the same directory with the original name."),
-            Err(e) => standard_io_error_handling(e),
-        }
-    } else {
-        match encrypt_and_save(file, filename, key) {
-            Ok(_) => {
+    match crypt_and_save(file, filename, key, encrypted) {
+        Ok(_) => {
+            if encrypted {
+                println!("Success! You can find the decrypted file in the same directory with the original or chosen name.");
+            } else {
                 println!("Success! You can find the encrypted file in the same directory with a .encrypt suffix");
                 println!("Advise: You can now test to decrypt the file again and then should delete the both unencrypted files.");
                 println!("Otherwise you whould have just wasted your time and no one wants to waist their time.");
             }
-            Err(e) => standard_io_error_handling(e),
         }
+        Err(e) => standard_io_error_handling(e),
     }
 }
 
@@ -46,32 +43,24 @@ fn get_args() -> Result<(File, String, u8, bool), io::Error> {
     Ok((File::open(&file)?, file, key, encrypted))
 }
 
-fn encrypt_and_save(f: File, mut name: String, key: u8) -> Result<(), io::Error> {
+fn crypt_and_save(f: File, mut name: String, key: u8, encrypted: bool) -> Result<(), io::Error> {
     let buf_reader = BufReader::new(f);
-    name.push_str(".encrypt");
+    if encrypted {
+        name = name.trim_right_matches(".encrypt").to_string();
+    } else {
+        name.push_str(".encrypt");
+    }
     match check_file_existance(&name) {
         Some(s) => name = s,
         None => (),
     }
     let mut buffer = BufWriter::new(File::create(name)?);
     for byte in buf_reader.bytes() {
-        buffer.write(&[encrypt(byte?, key)])?;
-    }
-    buffer.flush()?;
-    Ok(())
-}
-
-fn decrypt_and_save(f: File, mut name: String, key: u8) -> Result<(), io::Error> {
-    let buf_reader = BufReader::new(f);
-    name = name.trim_right_matches(".encrypt").to_string();
-    println!("{}", name);
-    match check_file_existance(&name) {
-        Some(s) => name = s,
-        None => (),
-    }
-    let mut buffer = BufWriter::new(File::create(name)?);
-    for byte in buf_reader.bytes() {
-        buffer.write(&[decrypt(byte?, key)])?;
+        if encrypted {
+            buffer.write(&[decrypt(byte?, key)])?;
+        } else {
+            buffer.write(&[encrypt(byte?, key)])?;
+        }
     }
     buffer.flush()?;
     Ok(())
@@ -106,7 +95,10 @@ fn check_file_existance(name: &String) -> Option<String> {
                     println!("Note that this time there won't be a check if the file already exists.");
                     input.clear();
                     match io::stdin().read_line(&mut input) {
-                        Ok(_) => return Some(input),
+                        Ok(_) => {
+                            println!("{}", input);
+                            return Some(input.trim_right().to_string());
+                        }
                         Err(e) => {
                             println!("ERROR: {}", e);
                             exit(1);

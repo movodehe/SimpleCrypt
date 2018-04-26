ca0009c5111862313abdd73e36f0abe3bfb66770
wholl0p <burch.samuel@gmail.com>
2018-04-26 15:13:34 +0200

    Added graphical welcome-screen

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,16 @@ mod unit_test_crypter;
 mod crypter;
 
 fn main() {
+	let version = env!("CARGO_PKG_VERSION");
+	println!("**************************************************************");
+	println!("*                                                            *");
+	println!("*                   Welcome to SimpleCrypt                   *");
+	println!("*                  by movodehe and wholl0p.                  *");
+	println!("*                                                            *");
+	println!("*                       version: {}                       *", version);
+	println!("*                                                            *");	
+	println!("*                  Usage: [file] [bitshift]                  *");
+	println!("*                                                            *");
+	println!("**************************************************************");
     crypter::run();
 }
